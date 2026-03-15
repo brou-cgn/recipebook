0785a98abe6ccf4eeb2dc4bb97e6d85db6346123
brou-cgn <benjamin.rousselli@googlemail.com>
2026-03-15 14:36:05 +0100

    Comment out title text in SplashScreen component
    Comment out title text and its usage in the splash screen.

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -6,7 +6,7 @@ function SplashScreen({ visible, logoUrl, appTitle, slogan }) {
   const [gone, setGone] = useState(false);
 
   const logoSrc = logoUrl || '/logo192.png';
-  /*const titleText = appTitle || 'brouBook';*/
+  //const titleText = appTitle || 'brouBook';
   const sloganText = slogan || 'Unsere besten Momente';
 
   useEffect(() => {
@@ -35,10 +35,10 @@ function SplashScreen({ visible, logoUrl, appTitle, slogan }) {
       <div className="splash-content">
         <img
           src={logoSrc}
-          alt={`${titleText} Logo`}
+          //alt={`${titleText} Logo`}
           className="splash-logo"
         />
-        /*<h1 className="splash-title">{titleText}</h1>*/
+        {/*<h1 className="splash-title">{titleText}</h1>*/}
         <p className="splash-slogan">{sloganText}</p>
       </div>
     </div>
